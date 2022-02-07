c3fa09a8f8f9fa533d22b4cfccabb7dc4949c258
adam12033 <90613920+adam12033@users.noreply.github.com>
2022-02-07 14:45:17 +0100

    Update cddb.sql

--- cddb.sql
+++ cddb.sql
@@ -1,4 +1,45 @@
---------cddb.sql----------
+---------------kekbt.sql----------------
+
+--database.sql
+--Délczeg Ádám
+--2022-02-02
+
+--Adatbázis létrehozása
+create database if not exists kekbt
+character set utf8
+collate utf8_hungarian_ci;
+
+use kekbt;
+
+-- Tábla létrehozása
+create table if not exists ugyfelek(
+	az int not null primary key auto_increment,
+	nev varchar(50),
+	telepules varchar(50),
+	vasarlas int
+);
+
+--Sorok beszúrása
+insert into ugyfelek
+(nev, telepules, vasarlas)
+values
+("Pontos Béla", "Szolnok", 2),
+("Sebes Irén", "Szeged", 1),
+("Nyolcas Imre", "Szolnok", 2),
+("Lenti Ferenc", "Miskolc", 1),
+("Fenti Katalin", "Hatvan", 1);
+
+
+
+
+
+
+
+
+
+
+
+---------------cddb.sql------------------
 
 -- nev
 -- datum
@@ -41,6 +82,9 @@ create table if not exists kiado(
 	  auto_increment,
 	nev varchar(50)
 ); 
+
+
+
 
 
 
