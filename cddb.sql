7a43f53cd5529966cce93d0134279316c296db4a
AndoAttila <andoattila698@gmail.com>
2022-02-07 14:32:28 +0100

    commit

---
+++ cddb.sql
@@ -0,0 +1,41 @@
+-- nev
+-- datum
+-- osztaly
+
+create database if not exists cdotthon
+character set utf8
+collate utf8_hungarian_ci;
+
+use cdotthon;
+
+create table if not exists  cd (
+	az int not null
+	  primary key
+	  auto_increment,
+	cim varchar(50),
+	kiadoAz int
+);
+
+create table if not exists  eloado (
+	az int not null
+	  primary key
+	  auto_increment,
+	nev varchar(50)
+);
+
+create table if not exists  zeneszam(
+	az int not null
+	  primary key
+	  auto_increment,
+	nev varchar(50),
+	hossz time,
+	cdAz int,
+	eloadoAz int  
+);
+
+create table if not exists kiado(
+	az int not null
+	  primary key
+	  auto_increment,
+	nev varchar(50)
+); 
